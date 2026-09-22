7d0cd5d88de8bbedcdab9eb3f32e50f4827797b4
Musabgpt <mgpt6zu@gmail.com>
2026-09-22 22:02:48 +0100

    feat: support internal system memory messages

--- app/src/main/java/com/musab/aragpt2/ChatMessage.java
+++ app/src/main/java/com/musab/aragpt2/ChatMessage.java
@@ -1,7 +1,16 @@
 package com.musab.aragpt2;
 
 public final class ChatMessage {
-    public static final int ROLE_USER=0, ROLE_ASSISTANT=1;
-    public final long id; public final int role; public final String text; public final long timeMs;
-    public ChatMessage(long id,int role,String text,long timeMs){this.id=id;this.role=role;this.text=text;this.timeMs=timeMs;}
+    public static final int ROLE_USER=0, ROLE_ASSISTANT=1, ROLE_SYSTEM=2;
+    public final long id;
+    public final int role;
+    public final String text;
+    public final long timeMs;
+
+    public ChatMessage(long id, int role, String text, long timeMs) {
+        this.id=id;
+        this.role=role;
+        this.text=text;
+        this.timeMs=timeMs;
+    }
 }
